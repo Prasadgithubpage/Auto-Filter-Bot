13ea228d00532aba56eda559b6a95520f6725b03
Prasadgithubpage <115395228+Prasadgithubpage@users.noreply.github.com>
2024-05-23 19:36:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-
 
 # Get a bot token from botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -13,19 +11,16 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 # Get from my.telegram.org (or @UseTGXBot)
 API_HASH = os.environ.get("API_HASH", "")
 
-
-
-
-# ID of Channel from which the bot shoul search files
+# ID of Channel from which the bot should search files
 MAINCHANNEL_ID = os.environ.get("MAINCHANNEL_ID", "")
-
-
-
 
 TG_BOT_SESSION = os.environ.get("TG_BOT_SESSION", "bot")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-LOG_FILE_NAME = "filterbot.txt"
 
+# Shorten the log file name
+LOG_FILE_NAME = "filterbot.log"
+
+# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -39,8 +34,10 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
+# Set the logging level for the Pyrogram library to WARNING
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
+# Define a LOGGER function to get a logger object with the specified name
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
